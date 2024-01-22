9140b79132d0bbada2b674e1beb2a8605925b4d1
Hayden <02hayden.coates@gulllakecs.org>
2024-01-22 09:46:55 -0500

    Strings

---
+++ Strings/first.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+#Programmer: Hayden Coates
+#Program: Strings
+
+print("Hi People \n")
+
+print("60 + 8 = ", 60+8)
+print(" \n 65554 + 876 ="+ str(65554+876) )
+
+print(""" \n Happy New Year!
+everyone, Yeah!!!""") #""" will print on multiple lines"""
