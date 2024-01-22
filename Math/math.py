17358e7aadda048f27dd817ab407ab02a47d6217
Hayden <02hayden.coates@gulllakecs.org>
2024-01-22 09:51:00 -0500

    Math

---
+++ Math/math.py
@@ -0,0 +1,12 @@
+#Programmer: Hayden Coates
+#Program: Math
+
+print(9 + 9) #Add
+print(9 * 9) #Multiply 
+print(9 - 2) #Subtraction
+print(9 // 3) #Divide
+print( 9 + 2 - 9 * 9 // 9 - 9) #PEMDAS
+print( 9 ** 3) #Exponet
+print( 50 % 3) #Modules 
+print( 50 / 6) #Divition with float
+print( 50 // 6) #Whole Number
